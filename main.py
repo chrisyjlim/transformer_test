e9e331fbbec24d4bca4315fb02fb702f1b3c9991
Chris <yewjee.lim@energyaustralia.com.au>
2024-11-10 21:06:31 +1100

    init

---
+++ main.py
@@ -0,0 +1,7 @@
+from transformers import pipeline 
+
+# Load a pre-trained text classification pipeline 
+classifier = pipeline('sentiment-analysis') 
+
+# Classify text 
+result = classifier('I love using the transformers library!')
